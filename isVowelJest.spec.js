da8f83ba7b1b6be4798f47415e49e86f2a6998e3
jnavarro <21sebas12@gmail.com>
2020-10-13 21:48:56 -0500

    set BDD (Behavior-driver developer)

--- isVowelJest.spec.js
+++ isVowelJest.spec.js
@@ -1,32 +1,15 @@
+
 const { isVowel } = require('.');
-const assert = require('assert');
 
-let failures = 0;
-const tests = [];
-//Regiters all test 
-const test = (name, fn) => { 
-    tests.push({name, fn})
-};
-
-test('isVowel return true "a" for input "a"', () => { 
-    assert.ok(isVowel('a'))
-})
-test('isVowel return false para input "C"', () => { 
-    assert.equal(isVowel('c'), false)
-})
-
-tests.forEach(({ name, fn}) => { 
-    console.log(name);
-    try {
-        fn()
-    } catch (error) {
-        console.error(`Expected ${error.expected} but got ${error.actual}` );
-        failures += 1;
-    }
+// BDD -> Behavio-driver developer.
+describe('isVowel', () => { 
+    it('should return true "a" for input "a"', () => { 
+        expect(isVowel('a')).toBe(true)
+    })
+    it('shoulds return false para input "C"', () => { 
+        expect(isVowel('c')).toBe(false)
+    })
+    
 })
 
 
-if (failures>0) { 
-    console.error(failures);
-    process.exit(1);
-}
