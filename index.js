a565d133cb193868cdc8c1c6177fb61f698d316e
jnavarro <21sebas12@gmail.com>
2020-10-13 22:37:10 -0500

    estableciendo axios

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const isVowel = (chart) => 'aeiou'.includes(chart)
+const isVowel = (chart) => {
+    if (typeof chart !== 'string') { 
+        throw TypeError('Arg must be a string') 
+    }
+    if (chart.length !== 1) { 
+        throw Error('Arg no is a only one vowel') 
+    }
+    return 'aeiou'.includes(chart)
+}
+const countVowels = (str) => [...str].reduce((memo, chart) => ( 
+                memo.hasOwnProperty(chart) ? { ...memo, [chart]: memo[chart] + 1 }
+                    : memo
+            ), { a: 0, e: 0, i: 0, o: 0, u: 0 }
+);
 module.exports = {
- isVowel
+    isVowel,
+    countVowels
 }
--- isVowelJest.spec.js
+++ isVowelJest.spec.js
@@ -1,15 +1,27 @@
 
-const { isVowel } = require('.');
+const { it } = require('@jest/globals');
+const { isVowel, countVowels } = require('.');
 
 // BDD -> Behavior-driver developer.
 describe('isVowel', () => { 
-    it.only('should return true "a" for input "a"', () => { 
+    it('debería arrojar error cuando se invoca sin string', () => { 
+        expect(() => isVowel()).toThrow(TypeError);
+    })
+    it('debería arrojar error cuando no es legth 1', () => { 
+        expect(() => isVowel('aaa')).toThrow('Arg no is a only one vowel');
+    })
+    it('should return true "a" for input "a"', () => { 
         expect(isVowel('a')).toBe(true)
     })
     it('shoulds return false para input "C"', () => { 
         expect(isVowel('c')).toBe(false)
     })
     
+})
+describe('countVowels', () => { 
+    it('should ...', () => {
+        expect(countVowels('abc')).toEqual({ a: 1, e: 0, i: 0, o: 0, u: 0 });
+    });
 })
 
 
