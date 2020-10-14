c541cb8accb679bb82ef54e87ff02282524f60d5
jnavarro <21sebas12@gmail.com>
2020-10-13 21:50:25 -0500

    skip a test

--- isVowelJest.spec.js
+++ isVowelJest.spec.js
@@ -1,9 +1,9 @@
 
 const { isVowel } = require('.');
 
-// BDD -> Behavio-driver developer.
+// BDD -> Behavior-driver developer.
 describe('isVowel', () => { 
-    it('should return true "a" for input "a"', () => { 
+    it.only('should return true "a" for input "a"', () => { 
         expect(isVowel('a')).toBe(true)
     })
     it('shoulds return false para input "C"', () => { 
